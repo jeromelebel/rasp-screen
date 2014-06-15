63d3528fbe363ad89db80f7b4d46421ee18b996e
Jérôme Lebel <jerome@fotonauts.com>
2014-06-15 15:40:11 +0200

    adding stuff for the touch screen and putting packages in /tmp/

--- install.sh
+++ install.sh
@@ -16,17 +16,18 @@ ssh "pi@${server}" sudo apt-get upgrade -y
 
 if [ "1" = "1" ]; then
     ssh "root@${server}" mkdir -p /tmp/package
-    ssh "root@${server}" wget http://adafruit-download.s3.amazonaws.com/libraspberrypi-bin-adafruit.deb
-    ssh "root@${server}" wget http://adafruit-download.s3.amazonaws.com/libraspberrypi-dev-adafruit.deb
-    ssh "root@${server}" wget http://adafruit-download.s3.amazonaws.com/libraspberrypi-doc-adafruit.deb
-    ssh "root@${server}" wget http://adafruit-download.s3.amazonaws.com/libraspberrypi0-adafruit.deb
-    ssh "root@${server}" wget http://adafruit-download.s3.amazonaws.com/raspberrypi-bootloader-adafruit-112613.deb
-    ssh "root@${server}" dpkg -i -B *.deb
+    ssh "root@${server}" "cd /tmp/package ; wget http://adafruit-download.s3.amazonaws.com/libraspberrypi-bin-adafruit.deb"
+    ssh "root@${server}" "cd /tmp/package ; wget http://adafruit-download.s3.amazonaws.com/libraspberrypi-dev-adafruit.deb"
+    ssh "root@${server}" "cd /tmp/package ; wget http://adafruit-download.s3.amazonaws.com/libraspberrypi-doc-adafruit.deb"
+    ssh "root@${server}" "cd /tmp/package ; wget http://adafruit-download.s3.amazonaws.com/libraspberrypi0-adafruit.deb"
+    ssh "root@${server}" "cd /tmp/package ; wget http://adafruit-download.s3.amazonaws.com/raspberrypi-bootloader-adafruit-112613.deb"
+    ssh "root@${server}" dpkg -i -B /tmp/package/*.deb
     ssh "root@${server}" rm -f /usr/share/X11/xorg.conf.d/99-fbturbo.conf
     ssh "root@${server}" "grep spi-bcm2708 /etc/modules || echo spi-bcm2708 >> /etc/modules"
     ssh "root@${server}" "grep fbtft_device /etc/modules || echo fbtft_device >> /etc/modules"
     ssh "root@${server}" "echo 'options fbtft_device name=adafruitts rotate=90 frequency=32000000' >> /etc/modprobe.d/adafruit.conf"
     ssh "root@${server}" "mkdir -p /etc/X11/xorg.conf.d"
+    ssh "root@${server}" "echo 'SUBSYSTEM==\"input\", ATTRS{name}==\"stmpe-ts\", ENV{DEVNAME}==\"*event*\", SYMLINK+=\"input/touchscreen\"' > /etc/udev/rules.d/95-stmpe.rules"
     ssh "root@${server}" reboot
     sleep 20
 fi
@@ -36,4 +37,4 @@ while [ "$?" != "0" ]; do
     ssh "pi@${server}" true 2> /dev/null
 done
 
-ssh "root@${server}" apt-get install git python-dev python-setuptools avahi
+ssh "root@${server}" apt-get install git python-dev python-setuptools avahi-daemon avahi-dnsconfd evtest tslib libts-bin
